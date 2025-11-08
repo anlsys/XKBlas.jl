253e10be67451c768762b964350b67ffefba24c9
Romain Pereira <rpereira@anl.gov>
2025-11-08 05:04:19 +0000

    Added some missing synchronous kernels, and '_lazy' interfaces

--- src/krylov-synchronous.jl
+++ src/krylov-synchronous.jl
@@ -36,8 +36,8 @@ Krylov.kcopy!(n::Integer, y::Vector{T}, x::Vector{T}) where {T<:BLAS.BlasFloat} 
 Krylov.kcopy!(n::Integer, y::AbstractVector, x::AbstractVector) = XKBlas.copy(n, x, 1, y, 1)
 
 # no need to impl
-Krylov.kdiv!(n::Integer, x::AbstractVector{Complex{T}}, s::T) where T <: AbstractFloat = kscal!(n, one(T) / s, x)
-Krylov.kdiv!(n::Integer, x::AbstractVector{T}, s::T) where T <: FloatOrComplex = kscal!(n, one(T) / s, x)
+Krylov.kdiv!(n::Integer, x::AbstractVector{Complex{T}}, s::T) where T <: AbstractFloat = Krylov.kscal!(n, one(T) / s, x)
+Krylov.kdiv!(n::Integer, x::AbstractVector{T}, s::T) where T <: FloatOrComplex = Krylov.kscal!(n, one(T) / s, x)
 
 Krylov.kdivcopy!(n::Integer, y::AbstractVector{Complex{T}}, x::AbstractVector{Complex{T}}, s::T) where T <: AbstractFloat = XKBlas.divcopy(n, x, 1, y, 1, s)
 Krylov.kdivcopy!(n::Integer, y::AbstractVector{T}, x::AbstractVector{T}, s::T) where T <: FloatOrComplex = XKBlas.divcopy(n, x, 1, y, 1, s)
@@ -48,12 +48,21 @@ Krylov.kdot(n::Integer, x::Vector{T}, y::Vector{T}) where T <: BLAS.BlasReal = X
 
 Krylov.kfill!(x::AbstractArray{T}, val::T) where T <: FloatOrComplex = XKBlas.fill(length(x), x, val)
 
-# TODO
-# knorm(n::Integer, x::AbstractVector{T}) where T <: FloatOrComplex = norm(x)
-# knorm(n::Integer, x::Vector{T}) where T <: BLAS.BlasFloat = BLAS.nrm2(n, x, 1)
+function knrm2(n::Integer, x::Vector{T}) where T <: AbstractFloat
+    # TODO: xkblas do not yet support nrm2, use dot
+    # r = Ref{T}(0)
+    # XKBlas.nrm2(n, x, 1, r)
+    # return r[]
+    return kdotr(n, x, x)
+end
+Krylov.knorm(n::Integer, x::AbstractVector{T}) where T <: FloatOrComplex = knrm2(n, x)
+Krylov.knorm(n::Integer, x::Vector{T}) where T <: BLAS.BlasFloat = knrm2(n, x)
 
 # TODO
 # knorm_elliptic(n::Integer, x::AbstractVector{T}, y::AbstractVector{T}) where T <: FloatOrComplex = (x === y) ? knorm(n, x) : kdotr(n, x, y) |> sqrt
+
+# TODO
+# kmul and ldiv
 
 Krylov.kscal!(n::Integer, s::T, x::AbstractVector{Complex{T}}) where T <: AbstractFloat = XKBlas.scal(n, s, x, 1)
 Krylov.kscal!(n::Integer, s::T, x::AbstractVector{T}) where T <: FloatOrComplex = XKBlas.scal(n, s, x, 1)
--- src/wrappers.jl
+++ src/wrappers.jl
@@ -42,6 +42,11 @@ axpy(n, alpha::Float64,    x, incx, y, incy)  = daxpy(n, Ref(alpha), x, incx, y,
 axpy(n, alpha::ComplexF32, x, incx, y, incy)  = caxpy(n, Ref(alpha), x, incx, y, incy)
 axpy(n, alpha::ComplexF64, x, incx, y, incy)  = zaxpy(n, Ref(alpha), x, incx, y, incy)
 
+axpy_lazy(n, alpha::Float32,    x, incx, y, incy)  = saxpy_lazy(n, Ref(alpha), x, incx, y, incy)
+axpy_lazy(n, alpha::Float64,    x, incx, y, incy)  = daxpy_lazy(n, Ref(alpha), x, incx, y, incy)
+axpy_lazy(n, alpha::ComplexF32, x, incx, y, incy)  = caxpy_lazy(n, Ref(alpha), x, incx, y, incy)
+axpy_lazy(n, alpha::ComplexF64, x, incx, y, incy)  = zaxpy_lazy(n, Ref(alpha), x, incx, y, incy)
+
 axpy_async(n, alpha::Float32,    x, incx, y, incy)  = saxpy_async(n, Ref(alpha), x, incx, y, incy)
 axpy_async(n, alpha::Float64,    x, incx, y, incy)  = daxpy_async(n, Ref(alpha), x, incx, y, incy)
 axpy_async(n, alpha::ComplexF32, x, incx, y, incy)  = caxpy_async(n, Ref(alpha), x, incx, y, incy)
@@ -51,6 +56,11 @@ axpby(n, alpha::Float32,    x, incx, beta, y, incy) = saxpby(n, Ref(alpha), x, i
 axpby(n, alpha::Float64,    x, incx, beta, y, incy) = daxpby(n, Ref(alpha), x, incx, Ref(beta), y, incy)
 axpby(n, alpha::ComplexF32, x, incx, beta, y, incy) = caxpby(n, Ref(alpha), x, incx, Ref(beta), y, incy)
 axpby(n, alpha::ComplexF64, x, incx, beta, y, incy) = zaxpby(n, Ref(alpha), x, incx, Ref(beta), y, incy)
+
+axpby_lazy(n, alpha::Float32,    x, incx, beta, y, incy) = saxpby_lazy(n, Ref(alpha), x, incx, Ref(beta), y, incy)
+axpby_lazy(n, alpha::Float64,    x, incx, beta, y, incy) = daxpby_lazy(n, Ref(alpha), x, incx, Ref(beta), y, incy)
+axpby_lazy(n, alpha::ComplexF32, x, incx, beta, y, incy) = caxpby_lazy(n, Ref(alpha), x, incx, Ref(beta), y, incy)
+axpby_lazy(n, alpha::ComplexF64, x, incx, beta, y, incy) = zaxpby_lazy(n, Ref(alpha), x, incx, Ref(beta), y, incy)
 
 axpby_async(n, alpha::Float32,    x, incx, beta, y, incy) = saxpby_async(n, Ref(alpha), x, incx, Ref(beta), y, incy)
 axpby_async(n, alpha::Float64,    x, incx, beta, y, incy) = daxpby_async(n, Ref(alpha), x, incx, Ref(beta), y, incy)
@@ -62,14 +72,22 @@ copy(n, x::AbstractVector{Float64},    incx, y::AbstractVector{Float64}   , incy
 copy(n, x::AbstractVector{ComplexF32}, incx, y::AbstractVector{ComplexF32}, incy)  = ccopy(n, x, incx, y, incy)
 copy(n, x::AbstractVector{ComplexF64}, incx, y::AbstractVector{ComplexF64}, incy)  = zcopy(n, x, incx, y, incy)
 
-copy_async(n, x::AbstractVector{Float32},   incx, y::AbstractVector{Float32}   , incy)  = scopy_async(n, x, y)
-copy_async(n, x::AbstractVector{Float64},   incx, y::AbstractVector{Float64}   , incy)  = dcopy_async(n, x, y)
-copy_async(n, x::AbstractVector{ComplexF32},incx, y::AbstractVector{ComplexF32}, incy)  = ccopy_async(n, x, y)
-copy_async(n, x::AbstractVector{ComplexF64},incx, y::AbstractVector{ComplexF64}, incy)  = zcopy_async(n, x, y)
+copy_lazy(n, x::AbstractVector{Float32},   incx, y::AbstractVector{Float32}   , incy)  = scopy_lazy(n, x, incx, y, incy)
+copy_lazy(n, x::AbstractVector{Float64},   incx, y::AbstractVector{Float64}   , incy)  = dcopy_lazy(n, x, incx, y, incy)
+copy_lazy(n, x::AbstractVector{ComplexF32},incx, y::AbstractVector{ComplexF32}, incy)  = ccopy_lazy(n, x, incx, y, incy)
+copy_lazy(n, x::AbstractVector{ComplexF64},incx, y::AbstractVector{ComplexF64}, incy)  = zcopy_lazy(n, x, incx, y, incy)
+
+copy_async(n, x::AbstractVector{Float32},   incx, y::AbstractVector{Float32}   , incy)  = scopy_async(n, x, incx, y, incy)
+copy_async(n, x::AbstractVector{Float64},   incx, y::AbstractVector{Float64}   , incy)  = dcopy_async(n, x, incx, y, incy)
+copy_async(n, x::AbstractVector{ComplexF32},incx, y::AbstractVector{ComplexF32}, incy)  = ccopy_async(n, x, incx, y, incy)
+copy_async(n, x::AbstractVector{ComplexF64},incx, y::AbstractVector{ComplexF64}, incy)  = zcopy_async(n, x, incx, y, incy)
 
 # TODO: complex version not supported yet, but they will need to change the dispatcher
 dot(n, x, incx, y, incy, result::Ref{Float32}) = sdot(n, x, incx, y, incy, result)
 dot(n, x, incx, y, incy, result::Ref{Float64}) = ddot(n, x, incx, y, incy, result)
+
+dot_lazy(n, x, incx, y, incy, result::Ref{Float32}) = sdot_lazy(n, x, incx, y, incy, result)
+dot_lazy(n, x, incx, y, incy, result::Ref{Float64}) = ddot_lazy(n, x, incx, y, incy, result)
 
 dot_async(n, x, incx, y, incy, result::Ref{Float32}) = sdot_async(n, x, incx, y, incy, result)
 dot_async(n, x, incx, y, incy, result::Ref{Float64}) = ddot_async(n, x, incx, y, incy, result)
@@ -79,14 +97,22 @@ fill(n, x, value::Float64   ) = dfill(n, x, value)
 fill(n, x, value::ComplexF32) = cfill(n, x, value)
 fill(n, x, value::ComplexF64) = zfill(n, x, value)
 
-fill_async(n, x, alpha::Float32   ) = sfill_async(n, x, value)
-fill_async(n, x, alpha::Float64   ) = dfill_async(n, x, value)
-fill_async(n, x, alpha::ComplexF32) = cfill_async(n, x, value)
-fill_async(n, x, alpha::ComplexF64) = zfill_async(n, x, value)
+fill_lazy(n, x, value::Float32   ) = sfill_lazy(n, x, value)
+fill_lazy(n, x, value::Float64   ) = dfill_lazy(n, x, value)
+fill_lazy(n, x, value::ComplexF32) = cfill_lazy(n, x, value)
+fill_lazy(n, x, value::ComplexF64) = zfill_lazy(n, x, value)
+
+fill_async(n, x, value::Float32   ) = sfill_async(n, x, value)
+fill_async(n, x, value::Float64   ) = dfill_async(n, x, value)
+fill_async(n, x, value::ComplexF32) = cfill_async(n, x, value)
+fill_async(n, x, value::ComplexF64) = zfill_async(n, x, value)
 
 # TODO: complex version not supported yet, but they will need to change the dispatcher
 scal(n, alpha::Float32, x, incx) = sscal_async(n, Ref(alpha), x, incx)
 scal(n, alpha::Float64, x, incx) = dscal_async(n, Ref(alpha), x, incx)
+
+scal_lazy(n, alpha::Float32, x, incx) = sscal_lazy(n, Ref(alpha), x, incx)
+scal_lazy(n, alpha::Float64, x, incx) = dscal_lazy(n, Ref(alpha), x, incx)
 
 scal_async(n, alpha::Float32, x, incx) = sscal_async(n, Ref(alpha), x, incx)
 scal_async(n, alpha::Float64, x, incx) = dscal_async(n, Ref(alpha), x, incx)
@@ -95,6 +121,9 @@ scal_async(n, alpha::Float64, x, incx) = dscal_async(n, Ref(alpha), x, incx)
 
 gemv(transA, m, n, alpha::Float32, A, lda, x, incx, beta, y, incy) = sgemv(transA, m, n, Ref(alpha), A, lda, x, incx, Ref(beta), y, incy)
 gemv(transA, m, n, alpha::Float64, A, lda, x, incx, beta, y, incy) = dgemv(transA, m, n, Ref(alpha), A, lda, x, incx, Ref(beta), y, incy)
+
+gemv_lazy(transA, m, n, alpha::Float32, A, lda, x, incx, beta, y, incy) = sgemv_lazy(transA, m, n, Ref(alpha), A, lda, x, incx, Ref(beta), y, incy)
+gemv_lazy(transA, m, n, alpha::Float64, A, lda, x, incx, beta, y, incy) = dgemv_lazy(transA, m, n, Ref(alpha), A, lda, x, incx, Ref(beta), y, incy)
 
 gemv_async(transA, m, n, alpha::Float32, A, lda, x, incx, beta, y, incy) = sgemv_async(transA, m, n, Ref(alpha), A, lda, x, incx, Ref(beta), y, incy)
 gemv_async(transA, m, n, alpha::Float64, A, lda, x, incx, beta, y, incy) = dgemv_async(transA, m, n, Ref(alpha), A, lda, x, incx, Ref(beta), y, incy)
@@ -106,6 +135,11 @@ gemm(transA, transB, m, n, k, alpha::Float64,    A, lda, B, ldb, beta::Float64, 
 gemm(transA, transB, m, n, k, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = cgemm(transA, transB, m, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 gemm(transA, transB, m, n, k, alpha::ComplexF64, A, lda, B, ldb, beta::ComplexF64, C, ldc)  = zgemm(transA, transB, m, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 
+gemm_lazy(transA, transB, m, n, k, alpha::Float32,    A, lda, B, ldb, beta::Float32,    C, ldc)  = sgemm_lazy(transA, transB, m, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+gemm_lazy(transA, transB, m, n, k, alpha::Float64,    A, lda, B, ldb, beta::Float64,    C, ldc)  = dgemm_lazy(transA, transB, m, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+gemm_lazy(transA, transB, m, n, k, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = cgemm_lazy(transA, transB, m, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+gemm_lazy(transA, transB, m, n, k, alpha::ComplexF64, A, lda, B, ldb, beta::ComplexF64, C, ldc)  = zgemm_lazy(transA, transB, m, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
 gemm_async(transA, transB, m, n, k, alpha::Float32,    A, lda, B, ldb, beta::Float32,    C, ldc)  = sgemm_async(transA, transB, m, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 gemm_async(transA, transB, m, n, k, alpha::Float64,    A, lda, B, ldb, beta::Float64,    C, ldc)  = dgemm_async(transA, transB, m, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 gemm_async(transA, transB, m, n, k, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = cgemm_async(transA, transB, m, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
@@ -116,25 +150,77 @@ gemmt_async(uplo, transA, transB, n, k, alpha::Float64,    A, lda, B, ldb, beta:
 gemmt_async(uplo, transA, transB, n, k, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = cgemmt_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 gemmt_async(uplo, transA, transB, n, k, alpha::ComplexF64, A, lda, B, ldb, beta::ComplexF64, C, ldc)  = zgemmt_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 
+herk(uplo, trans, n, k, alpha::Float32,    A, lda, beta::Float32,    C, ldc)  = sherk(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+herk(uplo, trans, n, k, alpha::Float64,    A, lda, beta::Float64,    C, ldc)  = dherk(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+herk(uplo, trans, n, k, alpha::ComplexF32, A, lda, beta::ComplexF32, C, ldc)  = cherk(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+herk(uplo, trans, n, k, alpha::ComplexF64, A, lda, beta::ComplexF64, C, ldc)  = zherk(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
 herk_async(uplo, trans, n, k, alpha::Float32,    A, lda, beta::Float32,    C, ldc)  = sherk_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 herk_async(uplo, trans, n, k, alpha::Float64,    A, lda, beta::Float64,    C, ldc)  = dherk_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 herk_async(uplo, trans, n, k, alpha::ComplexF32, A, lda, beta::ComplexF32, C, ldc)  = cherk_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 herk_async(uplo, trans, n, k, alpha::ComplexF64, A, lda, beta::ComplexF64, C, ldc)  = zherk_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
+herk_lazy(uplo, trans, n, k, alpha::Float32,    A, lda, beta::Float32,    C, ldc)  = sherk_lazy(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+herk_lazy(uplo, trans, n, k, alpha::Float64,    A, lda, beta::Float64,    C, ldc)  = dherk_lazy(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+herk_lazy(uplo, trans, n, k, alpha::ComplexF32, A, lda, beta::ComplexF32, C, ldc)  = cherk_lazy(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+herk_lazy(uplo, trans, n, k, alpha::ComplexF64, A, lda, beta::ComplexF64, C, ldc)  = zherk_lazy(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
+symm(side, uplo, m, n, alpha::Float32,    A, lda, B, ldb, beta::Float32,    C, ldc)  = ssymm(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+symm(side, uplo, m, n, alpha::Float64,    A, lda, B, ldb, beta::Float64,    C, ldc)  = dsymm(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+symm(side, uplo, m, n, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = csymm(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+symm(side, uplo, m, n, alpha::ComplexF64, A, lda, B, ldb, beta::ComplexF64, C, ldc)  = zsymm(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
+symm_lazy(side, uplo, m, n, alpha::Float32,    A, lda, B, ldb, beta::Float32,    C, ldc)  = ssymm_lazy(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+symm_lazy(side, uplo, m, n, alpha::Float64,    A, lda, B, ldb, beta::Float64,    C, ldc)  = dsymm_lazy(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+symm_lazy(side, uplo, m, n, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = csymm_lazy(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+symm_lazy(side, uplo, m, n, alpha::ComplexF64, A, lda, B, ldb, beta::ComplexF64, C, ldc)  = zsymm_lazy(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 
 symm_async(side, uplo, m, n, alpha::Float32,    A, lda, B, ldb, beta::Float32,    C, ldc)  = ssymm_async(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 symm_async(side, uplo, m, n, alpha::Float64,    A, lda, B, ldb, beta::Float64,    C, ldc)  = dsymm_async(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 symm_async(side, uplo, m, n, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = csymm_async(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 symm_async(side, uplo, m, n, alpha::ComplexF64, A, lda, B, ldb, beta::ComplexF64, C, ldc)  = zsymm_async(uplo, side, m, n, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 
+syr2k(uplo, trans, n, k, alpha::Float32,    A, lda, B, ldb, beta::Float32,    C, ldc)  = ssyr2k(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syr2k(uplo, trans, n, k, alpha::Float64,    A, lda, B, ldb, beta::Float64,    C, ldc)  = dsyr2k(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syr2k(uplo, trans, n, k, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = csyr2k(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syr2k(uplo, trans, n, k, alpha::ComplexF64, A, lda, B, ldb, beta::ComplexF64, C, ldc)  = zsyr2k(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
+syr2k_lazy(uplo, trans, n, k, alpha::Float32,    A, lda, B, ldb, beta::Float32,    C, ldc)  = ssyr2k_lazy(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syr2k_lazy(uplo, trans, n, k, alpha::Float64,    A, lda, B, ldb, beta::Float64,    C, ldc)  = dsyr2k_lazy(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syr2k_lazy(uplo, trans, n, k, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = csyr2k_lazy(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syr2k_lazy(uplo, trans, n, k, alpha::ComplexF64, A, lda, B, ldb, beta::ComplexF64, C, ldc)  = zsyr2k_lazy(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
 syr2k_async(uplo, trans, n, k, alpha::Float32,    A, lda, B, ldb, beta::Float32,    C, ldc)  = ssyr2k_async(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 syr2k_async(uplo, trans, n, k, alpha::Float64,    A, lda, B, ldb, beta::Float64,    C, ldc)  = dsyr2k_async(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 syr2k_async(uplo, trans, n, k, alpha::ComplexF32, A, lda, B, ldb, beta::ComplexF32, C, ldc)  = csyr2k_async(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 syr2k_async(uplo, trans, n, k, alpha::ComplexF64, A, lda, B, ldb, beta::ComplexF64, C, ldc)  = zsyr2k_async(uplo, trans, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 
+syrk(uplo, trans, n, k, alpha::Float32,    A, lda, beta::Float32,    C, ldc)  = ssyrk(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syrk(uplo, trans, n, k, alpha::Float64,    A, lda, beta::Float64,    C, ldc)  = dsyrk(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syrk(uplo, trans, n, k, alpha::ComplexF32, A, lda, beta::ComplexF32, C, ldc)  = csyrk(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syrk(uplo, trans, n, k, alpha::ComplexF64, A, lda, beta::ComplexF64, C, ldc)  = zsyrk(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
+syrk_lazy(uplo, trans, n, k, alpha::Float32,    A, lda, beta::Float32,    C, ldc)  = ssyrk_lazy(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syrk_lazy(uplo, trans, n, k, alpha::Float64,    A, lda, beta::Float64,    C, ldc)  = dsyrk_lazy(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syrk_lazy(uplo, trans, n, k, alpha::ComplexF32, A, lda, beta::ComplexF32, C, ldc)  = csyrk_lazy(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+syrk_lazy(uplo, trans, n, k, alpha::ComplexF64, A, lda, beta::ComplexF64, C, ldc)  = zsyrk_lazy(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
 syrk_async(uplo, trans, n, k, alpha::Float32,    A, lda, beta::Float32,    C, ldc)  = ssyrk_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 syrk_async(uplo, trans, n, k, alpha::Float64,    A, lda, beta::Float64,    C, ldc)  = dsyrk_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 syrk_async(uplo, trans, n, k, alpha::ComplexF32, A, lda, beta::ComplexF32, C, ldc)  = csyrk_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
 syrk_async(uplo, trans, n, k, alpha::ComplexF64, A, lda, beta::ComplexF64, C, ldc)  = zsyrk_async(uplo, transA, transB, n, k, Ref(alpha), A, lda, B, ldb, Ref(beta), C, ldc)
+
+
+
+trmm(side, uplo, transA, diag, m, n, alpha::Float32,    A, lda, B, ldb)  = strmm(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
+trmm(side, uplo, transA, diag, m, n, alpha::Float64,    A, lda, B, ldb)  = dtrmm(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
+trmm(side, uplo, transA, diag, m, n, alpha::ComplexF32, A, lda, B, ldb)  = ctrmm(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
+trmm(side, uplo, transA, diag, m, n, alpha::ComplexF64, A, lda, B, ldb)  = ztrmm(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
+
+trmm_lazy(side, uplo, transA, diag, m, n, alpha::Float32,    A, lda, B, ldb)  = strmm_lazy(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
+trmm_lazy(side, uplo, transA, diag, m, n, alpha::Float64,    A, lda, B, ldb)  = dtrmm_lazy(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
+trmm_lazy(side, uplo, transA, diag, m, n, alpha::ComplexF32, A, lda, B, ldb)  = ctrmm_lazy(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
+trmm_lazy(side, uplo, transA, diag, m, n, alpha::ComplexF64, A, lda, B, ldb)  = ztrmm_lazy(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
 
 trmm_async(side, uplo, transA, diag, m, n, alpha::Float32,    A, lda, B, ldb)  = strmm_async(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
 trmm_async(side, uplo, transA, diag, m, n, alpha::Float64,    A, lda, B, ldb)  = dtrmm_async(side, uplo, transA, diag, m, n, Ref(alpha), A, lda, B, ldb)
